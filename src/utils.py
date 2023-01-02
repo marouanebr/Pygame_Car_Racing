ea25cf5b597bc30ad8c16b80794432965231f1f5
Ons Dokh <bint.dokh@gmail.com>
2023-01-03 00:40:57 +0100

    Tutorial 4: Levels, Main Menu and Finishing Touches

--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,7 @@ def blit_rotate_center(win, img, top_left, angle):
     # rotate the image without changing the original x and y
     new_rect = rotated_img.get_rect(center=img.get_rect(topleft=top_left).center)
     win.blit(rotated_img, new_rect.topleft)
+
+def blit_text_center(win, font, text):
+    render = font.render(text, 1, (200, 200, 200))
+    win.blit(render, (win.get_width()/2 - render.get_width() / 2, win.get_height()/2 - render.get_height()/2))    
